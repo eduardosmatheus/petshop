3ab20d42bdfc02e1ac41aa746778e521d70cf4e3
Lucas Kruger <lucas.kr1996@gmail.com>
2016-11-11 13:12:06 -0200

    Montando arquitetura para utilizar redux

--- public/index.js
+++ public/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <div>
-    Hello World!
-  </div>
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
   , document.querySelector('#container'));
