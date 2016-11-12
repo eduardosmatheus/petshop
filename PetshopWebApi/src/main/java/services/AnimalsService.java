654fb2e4fa939da7de6e14a2a66e951c21545161
eduardosmatheus <matheus-edu@outlook.com>
2016-11-12 12:33:27 -0200

    Validating empty search

--- PetshopWebApi/src/main/java/services/AnimalsService.java
+++ PetshopWebApi/src/main/java/services/AnimalsService.java
@@ -1,13 +1,15 @@
 package services;
 
-import java.util.Optional;
+import java.util.NoSuchElementException;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Stream;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import mocking.BaseRepo;
 import model.Animal;
 
@@ -16,24 +18,27 @@ import model.Animal;
  */
 @Path("animals")
 public class AnimalsService {
-    
+
     private final CopyOnWriteArrayList<Animal> ANIMALS = BaseRepo.getAnimals();
-    
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getIt() {
         return Response.ok(ANIMALS, MediaType.APPLICATION_JSON).build();
     }
-    
+
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response findById(@PathParam("id") int id) {
-        Optional<Animal> res = ANIMALS
-                .stream()
-                .filter(i -> i.getId() == id).findAny();
-        return Response.ok(res.orElse(new Animal()), MediaType.APPLICATION_JSON).build();
+        try {
+            Animal res = ANIMALS.stream().filter(i -> i.getId() == id).findFirst().get();
+            if (res == null) {
+                return Response.status(Status.NOT_FOUND).build();
+            }
+            return Response.ok(res, MediaType.APPLICATION_JSON).build();
+        } catch (NoSuchElementException e) {
+            return Response.ok("Animal not found!", MediaType.APPLICATION_JSON).build();
+        }
     }
-    
-    
 }
