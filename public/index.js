9a6aa5493faef7bb8c1b0af7f4cf5459631e31d6
Lucas Kruger <lucas.kr1996@gmail.com>
2016-11-13 13:02:54 -0200

    Corrigindo nomenclatura de arquivos para interpretação dos arquivos .scss

--- public/index.js
+++ public/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; 
+import ReactDOM from 'react-dom';
 
 import { configureStore } from './store/configureStore';
 import { Root } from './containers/Root';
 import routes from './Routes'
+
+import './styles/main.scss'
 
 const store = configureStore();
 
